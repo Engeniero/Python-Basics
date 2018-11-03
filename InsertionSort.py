588b4abd1af799a232e8f234f4c73e2a84c08c7f
Engeniero <34884380+Engeniero@users.noreply.github.com>
2018-11-03 15:35:40 +0100

    Update InsertionSort.py

--- InsertionSort.py
+++ InsertionSort.py
@@ -1,10 +1,6 @@
 
 
-x = int(input("How much numbers you want to insert?: "))
 n= [3,9,6,7,8]
-#for y in range(x):
-    #try:
-        #n.extend([int(input("Insert a number:"))])
 
 for c in range(0,len(n),+1):
     for z in range(c,0,-1):
@@ -12,6 +8,5 @@ for c in range(0,len(n),+1):
             print("if,pass")
             n[-1],n[z] = n[z],n[-1]
 
-    #except ValueError:
-        #print("Insert an Int Number")
+
 print(n)
